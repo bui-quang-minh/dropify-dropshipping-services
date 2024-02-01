c48726ec5b94e07756c10b5643681787d3caa37f
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-02-01 10:54:30 +0700

    Add

---
+++ Dropify/Dropify/Pages/Product/AllProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dropify.Pages.Product
+{
+    public class AllProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Dropify/Dropify/obj/Debug/net8.0/Dropify.AssemblyInfo.cs
+++ Dropify/Dropify/obj/Debug/net8.0/Dropify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f75bc5769cdd78276db7a38cb71deb90e59d271")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97ddc3ecf0cdb80f5eb03fb50b8becd868151b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
