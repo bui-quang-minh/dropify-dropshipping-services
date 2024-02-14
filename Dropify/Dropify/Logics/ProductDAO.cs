822ad8edc55f518705af3cc9a6dc272c9b3d4c88
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-02-14 11:28:31 +0700

    Commit work on Product detail progress bar and all product search bar

--- Dropify/Dropify/Logics/ProductDAO.cs
+++ Dropify/Dropify/Logics/ProductDAO.cs
@@ -49,5 +49,61 @@ namespace Dropify.Logics
                 return p.ProductId;
             }
         }
+<<<<<<< Updated upstream
+=======
+        public List<Product> GetProductByStatus(string status)
+        {
+            List<Product> products = new List<Product>();
+            using (var db = new prn211_dropshippingContext())
+            {
+               products = db.Products.Include(p => p.Category)
+              .Include(p => p.Supplier)
+              .Include(p => p.ProductDetails)
+              .Where(p => p.Status == status).ToList();
+
+               return products;
+            }
+        }
+        public List<Product> SearchProduct(String name, int cid, int sid)
+        {
+            List<Product> products = new List<Product>();
+            if (cid != -1 && sid != -1)
+            {
+                using (var db = new prn211_dropshippingContext())
+                {
+                    products = db.Products.Where(p => p.Name.Contains(name) && p.CategoryId == cid && p.SupplierId == sid).ToList();
+
+                    return products;
+                }
+            }
+            else if (cid != -1 && sid == -1)
+            {
+                using (var db = new prn211_dropshippingContext())
+                {
+                    products = db.Products.Where(p => p.Name.Contains(name) && p.CategoryId == cid).ToList();
+
+                    return products;
+                }
+            }
+            else if (sid != -1 && cid == -1)
+            {
+                using (var db = new prn211_dropshippingContext())
+                {
+                    products = db.Products.Where(p => p.Name.Contains(name) && p.SupplierId == sid).ToList();
+
+                    return products;
+                }
+            }
+            else
+            {
+                using (var db = new prn211_dropshippingContext())
+                {
+                    products = db.Products.Where(p => p.Name.Contains(name)).ToList();
+
+                    return products;
+                }
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
