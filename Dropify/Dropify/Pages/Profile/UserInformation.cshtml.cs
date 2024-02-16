6b237d9942814b3be50bc95eb703af5b42c391fa
tunght2202 <hathanhtung2202@gmail.com>
2024-02-16 20:59:05 +0700

    Commit UserInformation

---
+++ Dropify/Dropify/Pages/Profile/UserInformation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dropify.Pages.Profile
+{
+    public class UserInformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
