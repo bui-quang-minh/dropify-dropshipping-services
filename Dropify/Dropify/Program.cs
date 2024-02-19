2de05712c1c09ee92e07467b7ad697ef6838eaad
nightspears <antrungdh@gmail.com>
2024-02-19 20:42:04 +0700

    fix some bugs

--- Dropify/Dropify/Program.cs
+++ Dropify/Dropify/Program.cs
@@ -27,7 +27,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles();// to use wwwroot
 
 app.UseRouting();
 
--- Dropify/Dropify/obj/Debug/net8.0/Dropify.AssemblyInfo.cs
+++ Dropify/Dropify/obj/Debug/net8.0/Dropify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca731baa329482b00db840db169f33d139c3935d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d54ddb813aab51326643559c5dd1fda25cd7a04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
