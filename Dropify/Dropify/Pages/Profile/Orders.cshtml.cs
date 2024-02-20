5749fe6570654cce1ff8d55556b67515b015bc5e
anhdunggg <dung182003@gmail.com>
2024-02-20 20:27:47 +0700

    add page order

---
+++ Dropify/Dropify/Pages/Profile/Orders.cshtml.cs
@@ -0,0 +1,25 @@
+using Dropify.Logics;
+using Dropify.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dropify.Pages.Profile
+{
+    public class OrdersModel : BasePageModel
+    {
+        //[BindProperty]
+        //public OrderDetail orderDetail { get; set; }
+
+        //[BindProperty]
+        //public Models.Order Order { get; set; }
+        //public Dropify.Models.prn211_dropshippingContext con;
+        //public OrderDAO pd = new OrderDAO();
+        //public OrderModel(Dropify.Models.prn211_dropshippingContext context)
+        //{
+        //    con = context;
+        //}
+        public void OnGet()
+        {
+        }
+    }
+}
