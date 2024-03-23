c6e184afbb4e1fa21be4fb95c3fbe7fb6d83eace
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-03-23 07:20:47 +0700

    commit

--- Dropify/Dropify/obj/Debug/net7.0/Dropify.AssemblyInfo.cs
+++ Dropify/Dropify/obj/Debug/net7.0/Dropify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a765698a42a40ed04150b9f8ee255af22eab49fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d505b507161dde79b5fb70f2402d37b12b8740")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dropify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
